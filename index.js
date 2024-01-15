d4c2a2ca5b92497bdd1b18e8bfdb078d5413b9e4
Biyi Lawal <biyilawal90@gmail.com>
2024-01-15 09:18:55 +0100

    Playing around with servers and requests

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 //importing http module
 const http = require("http");
 
+const behaviour = (req, res) => {
+    console.log(req.method);
+    console.log(req.url);
+
+    if(req.method == "POST" && req.url == "/test"){
+        res.write("Hello from Post");
+    } else if(req.method == "GET" && req.url == "/"){
+        res.write("Hello from Get!!")
+    }
+    res.end()
+}
 //creating a server
-const server = http.createServer((req, res) => {
-    res.write("Hi, I'm Biyi");
-    res.end();
-});
+// const server = http.createServer((req, res) => {
+//     // res.write("Hi, I'm Biyi");
+//     // res.end();
+// });
+const server = http.createServer(behaviour);
 
 //listening
-server.listen(8900, null, null, () => {
+server.listen(8900, () => {
     console.log("The server is running at port 8900");
 });
