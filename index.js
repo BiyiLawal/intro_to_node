56eecc340b730e4296d83ade8eb08b4c55fadfa7
Biyi Lawal <biyilawal90@gmail.com>
2024-01-16 19:42:58 +0100

    made changes to show basic server

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
 //importing http module
 const http = require("http");
 
-const behaviour = (req, res) => {
-    console.log(req.method);
-    console.log(req.url);
 
-    if(req.method == "POST" && req.url == "/test"){
-        res.write("Hello from Post");
-    } else if(req.method == "GET" && req.url == "/"){
-        res.write("Hello from Get!!")
-    }
-    res.end()
-}
 //creating a server
-// const server = http.createServer((req, res) => {
-//     // res.write("Hi, I'm Biyi");
-//     // res.end();
-// });
-const server = http.createServer(behaviour);
+const server = http.createServer((req, res) => {
+    res.write("Hi, I'm Biyi");
+    res.end();
+});
 
 //listening
 server.listen(8900, () => {
